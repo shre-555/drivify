a6647ad6ef6706407d57c3afcd7c0679ef104df5
shre-555 <shreyakundu6782@gmail.com>
2024-10-21 11:27:45 +0530

    select state updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import React from "react";
- 
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
- 
-// We import all the components we need in our app
-import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
- 
+// src/App.js
+import React from 'react';
+import HomePage from './HomePage';
+
 const App = () => {
- return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
-     </Routes>
-   </div>
- );
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
 };
- 
+
 export default App;
